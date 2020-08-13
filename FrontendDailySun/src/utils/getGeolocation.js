df93a673c682fe26eac184954c60b796679d4d0b
devacran <migueeldrg@gmail.com>
2020-08-13 15:15:26 -0500

    Se soluciono el bug de la ubicacion

--- FrontendDailySun/src/utils/getGeolocation.js
+++ FrontendDailySun/src/utils/getGeolocation.js
@@ -1,33 +1,16 @@
-// const getGeolocation = async () => {
-//   if (!navigator.geolocation) {
-//     return new Error("Geolocation is not suportted");
-//   }
-//   try {
-//     const location = await new Promise((resolve, reject) => {
-//       navigator.geolocation.getCurrentPosition(resolve, reject);
-//     });
-//     console.log("LOcation is", location);
-//     return {
-//       lat: location.coords.latitude,
-//       lon: location.coords.longitude
-//     };
-//   } catch (error) {
-//     console.log("Geolocalizacion error", error);
-//     return {
-//       lat: 19.42847,
-//       lon: -99.12766
-//     };
-//   }
-// };
-// export default getGeolocation;
 const getGeolocation = async () => {
   if (!navigator.geolocation) {
     return new Error("Geolocation is not suportted");
   }
   try {
+    let ifNotResponse;
     const location = await new Promise((resolve, reject) => {
+      ifNotResponse = window.setTimeout(() => {
+        reject("Geolocation was not enabled");
+      }, 5000);
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
+    clearTimeout(ifNotResponse);
     return {
       lat: location.coords.latitude,
       lon: location.coords.longitude
